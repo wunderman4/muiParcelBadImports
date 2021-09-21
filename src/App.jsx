a442078615e31caea4c165fa8be031ab710d78dd
Christian Wunder <christian@Christian-Wunder.local>
2021-09-21 13:08:02 -0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button, Grid } from "@mui/material"; // Doesnt work
+
+// This works
+// import Button from "@mui/material/Button";
+// import Grid from "@mui/material/Grid";
+
+export const App = () => (
+  <Grid container>
+    <h1>Hello world!</h1>
+    <Button>Hello world!</Button>
+  </Grid>
+);
